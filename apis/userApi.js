e37906eb7ee0df6a99ca8dc767e53d2ce46d8501
Yogesh <yogesh.kumar@brightchamps.com>
2022-07-30 23:17:59 +0530

    minor changes

--- apis/userApi.js
+++ apis/userApi.js
@@ -1,12 +1,23 @@
 const { uploadImage } = require("../utils/postUtils")
-
+const fs = require('fs');
+const { default: imagemin } = require("imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
 exports.post = async (req, res) => {
   try {
-    console.log("--->", req.body)
-    const file = req.file
-    console.log("---->", file)
-    const result = await uploadImage(file)
-    return res.status(200).json({ image_link: result?.Location })
+    const files = await imagemin([`./uploads/${req.file.filename}`], {
+      destination: "./compressed",
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({
+          quality: [0.6, 0.8],
+        }),
+      ],
+    });
+    const fileContent = fs.readFileSync("./compressed/" + req.file.filename);
+    const result = await uploadImage(fileContent, req.file.filename)
+    console.log("please check result")
+
   } catch (err) {
     res.status(400).json({ error: "something went wrong"})
   }
--- utils/postUtils.js
+++ utils/postUtils.js
@@ -2,17 +2,36 @@ const fs = require('fs')
 const { S3Instance } = require('../DB/S3/S3Instance')
 const multer = require('multer')
 const  upload = multer({ dest: 'uploads/' })
+const imagemin = require("imagemin");
 
-exports.imageMiddleware = (req, res, next) => {
-    upload.single('image')
-    next()
-}
-exports.uploadImage = (file) => {
-    const fileStream = fs.createReadStream(file.path)
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+      let originalname = file.originalname;
+      let ext = path.extname(originalname);
+      let newfilename = uuidv4() + ext;
+      cb(null, newfilename);
+    },
+  });
+
+exports.imageMiddleware = multer({
+    storage: storage,
+    limits: { fileSize: 7 * 1024 * 1024 },
+    fileFilter: function (req, file, callback) {
+      if (!file.originalname.match(/\.(png|jpeg|jpg|JPEG|JPG|PNG)$/)) {
+        return callback(new Error("Please upload png or jpg Image"));
+      }
+      callback(undefined, true);
+    },
+  }).single("profile");
+
+exports.uploadImage = (file, fileName) => {
     const uploadParams = {
         Bucket: bucketName,
-        Body: fileStream,
-        Key: file.fileName
+        Body: file,
+        Key: fileName
     }
     return S3Instance.upload(uploadParams).promise()
 }
