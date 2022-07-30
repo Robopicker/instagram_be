35f976f80dc7d9995d484ca9fcd28ea16cba56d5
Yogesh <yogesh.kumar@brightchamps.com>
2022-07-30 22:33:26 +0530

    pp

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { post } = require("../apis/userApi");
+const { imageMiddleware } = require("../utils/postUtils");
 const router = express.Router();
 const { runValidation } = require("../validators/runValidations");
 router.post('/post', runValidation, imageMiddleware ,post)
